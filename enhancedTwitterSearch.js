ab6a1283b6b7890fcf502c8867e6775fad0daf34
Dario Sanchez <dario@hireanesquire.com>
2022-11-24 16:40:37 -0500

    Reorganize sections

--- enhancedTwitterSearch.js
+++ enhancedTwitterSearch.js
@@ -1,33 +1,52 @@
 const enhancedTwitterSearch = () => {
+  // INITIALIZATIONS
+  let css = "";
+
   // UTILITIES
   const querySelector = document.querySelector.bind(document);
   const createElement = document.createElement.bind(document);
   const createTextNode = document.createTextNode.bind(document);
 
-  // IDs
-  const inputId = "[data-testid=SearchBox_Search_Input]";
-  const dropdownId = "[id=typeaheadDropdown-5]";
-  const emptyDropdownId = "[data-testid=typeaheadEmptySearch]";
-  const dropdownResultId = "[data-testid=typeaheadResult";
-
   // HTML ELEMENTS
   const headEl = document.head;
-  const inputEl = querySelector(inputId);
-  const dropdownEl = querySelector(dropdownId);
-  const emptyDropdownEl = querySelector(emptyDropdownId);
-  const dropdownResultEl = querySelector(dropdownResultId);
+
   const styleEl = createElement("style");
 
-  // STYLES
-  headEl.appendChild(styleEl);
-  const css = `
-    ${inputId} { 
-      padding: 8px;
+  const searchId = "[role=search]";
+  const searchEl = querySelector(searchId);
+
+  const inputId = "[data-testid=SearchBox_Search_Input]";
+  const inputEl = querySelector(inputId);
+  inputEl.placeholder = "Enhanced Search";
+  css += ``;
+  inputEl.addEventListener("input", (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.includes("from:")) {
+      console.log({ inputValue, index: inputValue.indexOf("from:") });
+      inputEl.value = inputValue.replace("from:", "potato:");
     }
-  `;
+  });
+
+  const listboxId = "[role=listbox]";
+  const listboxEl = querySelector(listboxId);
+  css += ``;
+
+  const emptyDropdownId = "[data-testid=typeaheadEmptySearch]";
+  const emptyDropdownEl = querySelector(emptyDropdownId);
+  css += ``;
+
+  const dropdownResultId = "[data-testid=typeaheadResult";
+  const dropdownResultEl = querySelector(dropdownResultId);
+
+  const userId = "[data-testid=TypeaheadUser";
+  const userEl = querySelector(userId);
+
+  // STYLES
+  css += ``;
+  headEl.appendChild(styleEl);
   styleEl.appendChild(createTextNode(css)); // https://stackoverflow.com/a/524721
 
   // CONSOLE GREETING
-  return "Hello, enhanced Twitter!";
+  return "✨ Hello, enhanced Twitter ✨";
 };
 enhancedTwitterSearch();
