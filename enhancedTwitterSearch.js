1f4b736f4caf019467edbe48c00a1d925deecfcf
Dario Sanchez <dario@hireanesquire.com>
2022-11-24 13:30:02 -0500

    Add more selectors and styles element for css

--- enhancedTwitterSearch.js
+++ enhancedTwitterSearch.js
@@ -1,7 +1,33 @@
 const enhancedTwitterSearch = () => {
-  console.log("Hello, enhanced Twitter!");
-  const searchInput = document.querySelector(
-    "[data-testid=SearchBox_Search_Input]"
-  );
+  // UTILITIES
+  const head = document.head;
+  const querySelector = document.querySelector.bind(document);
+  const createElement = document.createElement.bind(document);
+  const createTextNode = document.createTextNode.bind(document);
+
+  // IDs
+  const inputId = "[data-testid=SearchBox_Search_Input]";
+  const dropdownId = "[id=typeaheadDropdown-5]";
+  const emptyDropdownId = "[data-testid=typeaheadEmptySearch]";
+  const dropdownResultId = "[data-testid=typeaheadResult";
+
+  // HTML ELEMENTS
+  const inputEl = querySelector(inputId);
+  const dropdownEl = querySelector(dropdownId);
+  const emptyDropdownEl = querySelector(emptyDropdownId);
+  const dropdownResultEl = querySelector(dropdownResultId);
+
+  // STYLES
+  const style = createElement("style");
+  head.appendChild(style);
+  const css = `
+    ${inputId} { 
+      padding: 8px;
+    }
+  `;
+  style.appendChild(createTextNode(css)); // https://stackoverflow.com/a/524721
+
+  // CONSOLE GREETING
+  return "Hello, enhanced Twitter!";
 };
 enhancedTwitterSearch();
