05a1d58789a42d0db4b12d195f12f56cc44c91ff
Dario Sanchez <dario@hireanesquire.com>
2022-11-24 10:37:09 -0500

    Add base function

---
+++ enhancedTwitterSearch.js
@@ -0,0 +1,7 @@
+const enhancedTwitterSearch = () => {
+  console.log("Hello, enhanced Twitter!");
+  const searchInput = document.querySelector(
+    "[data-testid=SearchBox_Search_Input]"
+  );
+};
+enhancedTwitterSearch();
